07247ecbd35840fdf997f694646c438b874065e3
takuchan69 <57449179+takuchan69@users.noreply.github.com>
2020-06-20 23:18:55 +0900

    made head title
    just statrted

--- index.php
+++ index.php
@@ -1,3 +1,23 @@
 <?php
 
 ?>
+<!DOCTYPE html>
+<html lang="ja">
+  <head>
+        <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width">
+    <meta name="description" content="website to introduce company profile">
+    <title>FIND18 LLC</title>
+    <link rel="stylesheet" href="find18.css">
+  </head>
+  <body>
+      <header>
+        <h2>合同会社FIND18</h2>
+    </header>
+      <section id="container">
+        
+    </section>
+    <footer>
+    </footer>
+  </body>
+</html>
